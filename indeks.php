0eaeaac49d6d3a5c317d6524bd24a54a7f60eec3
killuazoldyc05 <152873089+killuazoldyc05@users.noreply.github.com>
2023-12-04 19:36:46 +0700

    Add files via upload

---
+++ indeks.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>this is for you</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="wrapper">
+        <h2 class="question">you like me?</h2>
+        img class="gif" alt="gif"
+scr="https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gifyou.webp"
+    <div class="btn-group"> <button classs="yes-btn">yes</button> <button class="no-btn"></button>
+    </div>
+   </div>
+  <script scr="script.js"></script>
+</body>
+</html> 
